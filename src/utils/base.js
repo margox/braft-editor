2dcacfdf6b8a1064132fb60144a202b607a516d8
Margox <margox@foxmail.com>
2017-08-31 13:12:11 +0800

    解决冲突

--- src/utils/base.js
+++ src/utils/base.js
@@ -9,20 +9,3 @@ export const UniqueIndex = () => {
   return window.__BRAFT_UNIQUE_INDEX__ 
 
 }
-
-export const isPromise = (obj) => {
-
-  return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function'
-
-}
-
-export const imageToURL = (param) => {
-
-  setTimeout(() => {
-    param.onSuccess({
-      url: URL.createObjectURL(param.file),
-      name: param.file.name
-    })
-  }, 1000)
-
-}
