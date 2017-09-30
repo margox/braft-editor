3c07db06df67856631c0343154c5926ccad0023a
Margox <margox@foxmail.com>
2017-09-30 13:44:58 +0800

    react-loader配置更新

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,7 +14,7 @@ module.exports = {
       }, {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['react-hot-loader', 'babel-loader']
+        use: ['babel-loader']
       }, {
         test: /\.(png|svg)$/,
         use: [
