0d909e834189955496d1bb4ff6af843a0abb6377
Paweł Krefta <pkrefta@users.noreply.github.com>
2018-10-20 14:33:29 +0200

    Polish translation & typo fix in english translation (#260)

---
+++ src/languages/pl.js
@@ -0,0 +1,72 @@
+export default {
+  base: {
+    remove: 'Usuń',
+    cancel: 'Anuluj',
+    confirm: 'Potwierdź',
+    inert: 'Wstaw',
+    width: 'Szerokość',
+    height: 'Wysokość'
+  },
+  controls: {
+    clear: 'Wyczyść',
+    undo: 'Cofnij',
+    redo: 'Przywróć',
+    fontSize: 'Wielkość',
+    color: 'Kolor',
+    textColor: 'Kolor tekstu',
+    tempColors: 'Kolory',
+    backgroundColor: 'Tło',
+    bold: 'Pogrubienie',
+    lineHeight:'Wysokość linii',
+    letterSpacing:'Odstęp znaków',
+    textIndent:'Wcięcie tekstu',
+    increaseIndent: 'Zwiększ wcięcie',
+    decreaseIndent: 'Zmniejsz wcięcie',
+    italic: 'Italiki',
+    underline: 'Podkreślenie',
+    strikeThrough: 'Przekreślenie',
+    fontFamily: 'Czcionka',
+    textAlign: 'Wyrównanie tekstu',
+    alignLeft: 'Do lewej',
+    alignCenter: 'Wycentruj',
+    alignRight: 'Do prawej',
+    alignJustify: 'Wyjustuj',
+    floatLeft: 'Do lewej',
+    floatRight: 'Do prawej',
+    superScript: 'Superskrypt',
+    subScript: 'Subskrypt',
+    removeStyles: 'Usuń stylowanie',
+    headings: 'Nagłówki',
+    header: 'Nagłówek',
+    normal: 'Normalny',
+    orderedList: 'Lista uporządkowana',
+    unorderedList: 'Lista nieuporządkowana',
+    blockQuote: 'Cytat',
+    code: 'Kod',
+    link: 'Link',
+    unlink: 'Usuń link',
+    hr: 'Linia pozioma',
+    media: 'Media',
+    mediaLibirary: 'Biblioteka mediów',
+    emoji: 'Emoji'
+  },
+  linkEditor: {
+    inputPlaceHolder: 'Adres URL',
+    inputWithEnterPlaceHolder: 'Wpisz adres URL i naciśnij Enter',
+    openInNewWindow: 'Otwórz w nowym oknie',
+    removeLink: 'Usuń link'
+  },
+  audioPlayer: {
+    title: 'Odtwórz audio'
+  },
+  videoPlayer: {
+    title: 'Odtwórz wideo',
+    embedTitle: 'Tytuł'
+  },
+  media: {
+    image: 'Obraz',
+    video: 'Wideo',
+    audio: 'Audio',
+    embed: 'Obiekt osadzony'
+  }
+}
