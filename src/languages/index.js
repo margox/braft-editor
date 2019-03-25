c597a451d150cf48d32df407a207769f6c9a7c6c
YYL1999 <2291003927@qq.com>
2019-03-25 10:26:59 +0800

    添加韩语和日语

--- src/languages/index.js
+++ src/languages/index.js
@@ -2,10 +2,14 @@ import en from './en'
 import zh from './zh'
 import zhHant from './zh-hant'
 import pl from './pl'
+import kr from './kr'
+import jpn from './jpn'
 
 export default {
   'en': en,
   'zh': zh,
   'zh-hant': zhHant,
   'pl': pl,
+  'kr':kr,
+  'jpn':jpn
 }
---
+++ src/languages/jpn.js
@@ -0,0 +1,75 @@
+export default {
+  base: {
+    remove: '削除',
+    cancel: 'キャンセル',
+    confirm: '決定',
+    inert: '挿入',
+    width: '幅',
+    height: '高さ'
+  },
+  controls: {
+    clear: 'クリアコンテンツ',
+    undo: 'キャンセル',
+    redo: 'キャンセル',
+    fontSize: 'フォントサイズ',
+    lineHeight: 'フォントサイズ',
+    letterSpacing: 'ワード間隔',
+    textIndent: '段落のインデント',
+    increaseIndent: 'インデントを増やす',
+    decreaseIndent: 'インデントを減らす',
+    border: '国境',
+    color: '色',
+    textColor: 'テキストの色',
+    backgroundColor: '背景色',
+    tempColors: '仮色',
+    bold: '太字',
+    italic: 'イタリック体',
+    underline: '下線',
+    strikeThrough: '取り消し線',
+    fontFamily: 'フォント',
+    textAlign: 'テキストの配置',
+    alignLeft: '左',
+    alignCenter: '中央揃え',
+    alignRight: '右に立つ',
+    alignJustify: '両端',
+    floatLeft: '左フローティング',
+    floatRight: '右フローティング',
+    superScript: '上付き',
+    subScript: '下付き文字',
+    removeStyles: 'クリアスタイル',
+    headings: '役職',
+    header: '役職',
+    normal: '従来の',
+    orderedList: '順序付きリスト',
+    unorderedList: '番号なしリスト',
+    blockQuote: '参照',
+    code: 'コード',
+    link: 'リンク',
+    unlink: 'リンクを解除',
+    hr: '横線',
+    media: 'メディア',
+    mediaLibirary: 'メディアライブラリー',
+    emoji: '小さな表現',
+    fullscreen: '全画面',
+    exitFullscreen: '全画面を退く',
+  },
+  linkEditor: {
+    inputPlaceHolder: 'リンクアドレスを入力',
+    inputWithEnterPlaceHolder: 'リンクアドレスを入力して戻ります',
+    openInNewWindow: '新しいウィンドウで開く',
+    removeLink: '新しいウィンドウで開く'
+  },
+  audioPlayer: {
+    title: 'オーディオファイルを再生する'
+  },
+  videoPlayer: {
+    title: 'ビデオファイルを再生する',
+    embedTitle: '埋め込みメディア'
+  },
+  media: {
+    image: '画像',
+    video: 'ビデオ',
+    audio: '音声',
+    embed: '埋め込みメディア'
+  }
+}
