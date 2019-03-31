81c630b5e5155e523525e395ba57728f754c0f1d
YYL1999 <39796460+YYL1999@users.noreply.github.com>
2019-03-31 14:10:51 +0800

    添加韩语和日语 (#417)

---
+++ src/languages/kr.js
@@ -0,0 +1,75 @@
+export default {
+  base: {
+    remove: '삭제',
+    cancel: '취소',
+    confirm: '결정',
+    inert: '삽입',
+    width: '너비',
+    height: '높이'
+  },
+  controls: {
+    clear: '콘텐츠지우기',
+    undo: '취소',
+    redo: '다시하기',
+    fontSize: '글자크기',
+    lineHeight: '행높이',
+    letterSpacing: '단어간격',
+    textIndent: '단락들여쓰기',
+    increaseIndent: '들여쓰기늘리기',
+    decreaseIndent: '들여쓰기줄이기',
+    border: '국경',
+    color: '색상',
+    textColor: '텍스트색상',
+    backgroundColor: '배경색상',
+    tempColors: '임시색',
+    bold: '굵게',
+    italic: '기울임꼴',
+    underline: '밑줄',
+    strikeThrough: '취소선',
+    fontFamily: '글꼴',
+    textAlign: '텍스트정렬',
+    alignLeft: '왼쪽',
+    alignCenter: '중심',
+    alignRight: '오른쪽',
+    alignJustify: '양쪽끝',
+    floatLeft: '떠다니기',
+    floatRight: '오른쪽부동',
+    superScript: '위첨자',
+    subScript: '첨자',
+    removeStyles: '스타일지우기',
+    headings: '제목',
+    header: '제목',
+    normal: '재래식',
+    orderedList: '순서가지정된목록',
+    unorderedList: '정렬되지않은목록',
+    blockQuote: '참고문헌',
+    code: '코드',
+    link: '링크',
+    unlink: '링크삭제',
+    hr: '수평선',
+    media: '미디어',
+    mediaLibirary: '미디어라이브러리',
+    emoji: '작은표현',
+    fullscreen: '전체화면',
+    exitFullscreen: '전체화면종료',
+  },
+  linkEditor: {
+    inputPlaceHolder: '링크주소입력',
+    inputWithEnterPlaceHolder: '링크주소입력.',
+    openInNewWindow: '새창열기',
+    removeLink: '링크삭제'
+  },
+  audioPlayer: {
+    title: '오디오파일재생'
+  },
+  videoPlayer: {
+    title: '비디오파일재생',
+    embedTitle: '임베디드미디어'
+  },
+  media: {
+    image: '이미지',
+    video: '비디오',
+    audio: '오디오',
+    embed: '임베디드미디어'
+  }
+}
