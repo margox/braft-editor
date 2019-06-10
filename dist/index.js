976a62af1ce6605ac73131aed541a690512c1fed
TheComedian08 <thecomedian08@gmail.com>
2019-06-10 14:49:09 +0300

    Added Russian language (#519)
    * added ru language, but translating isn't read
    
    I will translate this to Russian language
    
    * Update index.js
    
    * Added Russiang language
    
    * Create ru.js

--- dist/index.js
+++ dist/index.js
@@ -1496,6 +1496,80 @@ var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_)
     embed: 'Embed'
   }
 });
+var ru = ({
+        base: {
+          remove: 'Удалить',
+          cancel: 'Отмена',
+          confirm: 'Подтвердить',
+          insert: 'Вставить',
+          width: 'Ширина',
+          height: 'Высота'
+        },
+        controls: {
+          clear: 'Очистить',
+          undo: 'Отменить',
+          redo: 'Повторить',
+          fontSize: 'Размер шрифта',
+          color: 'Цвет',
+          textColor: 'Цвет текста',
+          tempColors: 'Temp Colors',
+          backgroundColor: 'Цвет фона',
+          bold: 'Жирный',
+          lineHeight: 'Межстрочный интервал',
+          letterSpacing: 'Межбуквенный интервал',
+          textIndent: 'Отступ',
+          increaseIndent: 'Увеличить отступ',
+          decreaseIndent: 'Уменьшить отступ',
+          italic: 'Курсив',
+          underline: 'Подчеркнутый',
+          strikeThrough: 'Перечеркнутый',
+          fontFamily: 'Шрифт',
+          textAlign: 'Расположение текста',
+          alignLeft: 'По левому краю',
+          alignCenter: 'По центру',
+          alignRight: 'По правому краю',
+          alignJustify: 'По ширине',
+          floatLeft: 'Обтекание слева',
+          floatRight: 'Обтекание справа',
+          superScript: 'Надстрочный индекс',
+          subScript: 'Подстрочный индекс',
+          removeStyles: 'Убрать стили',
+          headings: 'Заголовки',
+          header: 'Заголовок',
+          normal: 'Обычный',
+          orderedList: 'Упорядоченный список',
+          unorderedList: 'Неупорядоченный список',
+          blockQuote: 'Цитата',
+          code: 'Код',
+          link: 'Вставить ссылку',
+          unlink: 'Убрать ссылку',
+          hr: 'Горизонтальная линия',
+          media: 'Медиа',
+          mediaLibirary: 'Медиа библиотека',
+          emoji: 'Emoji',
+          fullscreen: 'Полноэкранный режим',
+          exitFullscreen: 'Выйти из полноэкранного режима'
+        },
+        linkEditor: {
+          inputPlaceHolder: 'Вставить ссылку',
+          inputWithEnterPlaceHolder: 'Вставить ссылку и нажать Enter',
+          openInNewWindow: 'Открыть в новом окне',
+          removeLink: 'Убрать ссылку'
+        },
+        audioPlayer: {
+          title: 'Воспроизвести аудиофайл'
+        },
+        videoPlayer: {
+          title: 'Воспроизвести видеофайл',
+          embedTitle: 'Embed Media'
+        },
+        media: {
+          image: 'Картинка',
+          video: 'Видео',
+          audio: 'Аудио',
+          embed: 'Встроенное'
+        }
+      });
 // CONCATENATED MODULE: ./languages/zh.js
 /* harmony default export */ var zh = ({
   base: {
@@ -1572,7 +1646,7 @@ var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_)
     embed: '嵌入式媒体'
   }
 });
-// CONCATENATED MODULE: ./languages/zh-hant.js
+// CONCATENATED MODULE: .s/zh-hant.js
 /* harmony default export */ var zh_hant = ({
   base: {
     remove: '刪除',
@@ -1886,7 +1960,8 @@ var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_)
   'zh-hant': zh_hant,
   'pl': pl,
   'kr': kr,
-  'jpn': jpn
+  'jpn': jpn,
+  'ru':ru
 });
 // EXTERNAL MODULE: external "braft-finder"
 var external_braft_finder_ = __webpack_require__(17);
