5ecc96ea8dc28a9b6bf3b54c2a8b7e57f08adacb
Gencer W. Genç <gencer@users.noreply.github.com>
2019-06-10 15:33:20 +0300

    Add Turkish language

---
+++ src/languages/tr.js
@@ -0,0 +1,74 @@
+export default {
+  base: {
+    remove: 'Kaldır',
+    cancel: 'İptal',
+    confirm: 'Onayla',
+    inert: 'Ekle',
+    width: 'Genişlik',
+    height: 'Yükseklik'
+  },
+  controls: {
+    clear: 'Temizle',
+    undo: 'Geri al',
+    redo: 'İleri al',
+    fontSize: 'Yazı boyutu',
+    color: 'Renk',
+    textColor: 'Yazı rengi',
+    tempColors: 'Geçici renkler',
+    backgroundColor: 'Arkaplan',
+    bold: 'Kalın',
+    lineHeight:'Satır yüksekliği',
+    letterSpacing:'Harf aralığı',
+    textIndent:'Çentik aralığı',
+    increaseIndent: 'Çentiği genişlet',
+    decreaseIndent: 'Çentiği daralt',
+    italic: 'Eğik',
+    underline: 'Altı çizili',
+    strikeThrough: 'Üstü çizili',
+    fontFamily: 'Yazı tipi',
+    textAlign: 'Metin Hizalama',
+    alignLeft: 'Sola hizala',
+    alignCenter: 'Ortaya hizala',
+    alignRight: 'Sağa hizala',
+    alignJustify: 'Her iki tarafa hizala',
+    floatLeft: 'Sola yatır',
+    floatRight: 'Sağa yatır',
+    superScript: 'Ana kod',
+    subScript: 'Alt kod',
+    removeStyles: 'Stilleri kaldır',
+    headings: 'Başlıklar',
+    header: 'Başlık',
+    normal: 'Normal',
+    orderedList: 'Sıralı liste',
+    unorderedList: 'Sırasız liste',
+    blockQuote: 'Alıntı',
+    code: 'Kod',
+    link: 'Bağlantı',
+    unlink: 'Bağlantıyı kaldır',
+    hr: 'Yatay çizgi',
+    media: 'Medya',
+    mediaLibirary: 'Kütüphane',
+    emoji: 'İfade',
+    fullscreen: 'Tam ekran',
+    exitFullscreen: 'Tam ekrandan çık',
+  },
+  linkEditor: {
+    inputPlaceHolder: 'Bağlantı URL\' si girin',
+    inputWithEnterPlaceHolder: 'Bağlantı URL\'si girin ve Enter' a basın',
+    openInNewWindow: 'Yeni pencerede aç',
+    removeLink: 'Bağlantıyı kaldır'
+  },
+  audioPlayer: {
+    title: 'Ses çal'
+  },
+  videoPlayer: {
+    title: 'Görüntü oynat',
+    embedTitle: 'Görüntüyü göm'
+  },
+  media: {
+    image: 'Resim',
+    video: 'Görüntü',
+    audio: 'Ses',
+    embed: 'Gömülü nesne'
+  }
+}
