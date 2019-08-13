aae5c9912847461bc2b6f564bed64d484a50f044
Rayron Victor <rayronvictor@gmail.com>
2019-08-13 11:09:40 -0300

    Add Brazilian Portuguese

---
+++ src/languages/pt-br.js
@@ -0,0 +1,75 @@
+export default {
+  base: {
+    remove: 'Remover',
+    cancel: 'Cancelar',
+    confirm: 'Confirmar',
+    inert: 'Inserir',
+    width: 'Largura',
+    height: 'Altura'
+  },
+  controls: {
+    clear: 'Limpar',
+    undo: 'Desfazer',
+    redo: 'Refazer',
+    fontSize: 'Tamanho da Fonte',
+    color: 'Cor',
+    textColor: 'Texto',
+    tempColors: 'Temp Colors',
+    backgroundColor: 'Cor de Fundo',
+    bold: 'Negrito',
+    lineHeight:'Altura da LinhaLine Height',
+    letterSpacing:'Espaçamento entre Letras',
+    textIndent:'Identação de Texto',
+    increaseIndent: 'Aumentar Identação',
+    decreaseIndent: 'Diminuir Identção',
+    italic: 'Itálico',
+    underline: 'Sublinhado',
+    strikeThrough: 'Riscado',
+    fontFamily: 'Família da Fonte',
+    textAlign: 'Alinhamento de Texto',
+    alignLeft: 'Alinhamento à Esquerda',
+    alignCenter: 'Alinhamento Centralizado',
+    alignRight: 'Alinhamento à Direita',
+    alignJustify: 'Alinhamento Justificado',
+    floatLeft: 'Flutuação à Esquerda',
+    floatRight: 'Flutuação à Direita',
+    superScript: 'Sobrescrito',
+    subScript: 'Subscrito',
+    removeStyles: 'Remover Estilos',
+    headings: 'Cabeçalhos',
+    header: 'Cabeçalho',
+    normal: 'Normal',
+    orderedList: 'Lista Ordenada',
+    unorderedList: 'Lista Não Ordenada',
+    blockQuote: 'Citação',
+    code: 'Código',
+    link: 'Link',
+    unlink: 'Remover Link',
+    hr: 'Linha Horizontal',
+    media: 'Mídia',
+    mediaLibirary: 'Biblioteca de Mídia',
+    emoji: 'Emoji',
+    fullscreen: 'Tela Cheia',
+    exitFullscreen: 'Sair de Tela Cheia',
+  },
+  linkEditor: {
+    textInputPlaceHolder: 'Insira o texto do link',
+    linkInputPlaceHolder: 'Insira a URL do link',
+    inputWithEnterPlaceHolder: 'Insira a URL do link e aperte Enter',
+    openInNewWindow: 'Abrir em nova janela',
+    removeLink: 'Remover Link'
+  },
+  audioPlayer: {
+    title: 'Tocar Áudio'
+  },
+  videoPlayer: {
+    title: 'Tocar Vídeo',
+    embedTitle: 'Mídia Embutida'
+  },
+  media: {
+    image: 'Imagem',
+    video: 'Vídeo',
+    audio: 'Áudio',
+    embed: 'Embutido'
+  }
+}
